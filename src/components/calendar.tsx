ea3c900ec485558253711bca102018d937c3b4e4
Pimchat Panichsukko <pim.pimchat@gmail.com>
2023-12-12 20:44:07 +0700

    add prev and next month functions to calendar

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,23 +1,23 @@
-import { startOfMonth, getDaysInMonth, getDay } from 'date-fns';
+import { startOfMonth, getDaysInMonth, getDay, addMonths, subMonths } from 'date-fns';
+import { useState } from "react";
 import './calendar.css';
+import ArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeftOutlined';
+import ArrowRightIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
 
-type month = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December';
-type day = 'Su' | 'Mo' | 'Tu' | 'We' | 'Th' | 'Fr' | 'Sa';
+type Month = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December';
+type Day = 'Su' | 'Mo' | 'Tu' | 'We' | 'Th' | 'Fr' | 'Sa';
 
-interface calendarConfig {
+interface CalendarConfig {
   startDay: number;
   month: number;
   year: number;
   daysInMonth: number;
 }
 
-const daysInWeek: day[] = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
-const months: month[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const daysInWeek: Day[] = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+const months: Month[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const todayDate = new Date(2112, 8, 3);
-console.log('input date : ', todayDate);
-
-function getCalendarConfig(date: Date): calendarConfig {
+function getCalendarConfig(date: Date): CalendarConfig { //to be revised
   const monthStart = startOfMonth(date);
   const daysInMonth = getDaysInMonth(monthStart);
   const year = monthStart.getFullYear();
@@ -38,41 +38,53 @@ function dayTemplate() {
   return divs;
 }
 
-function calendarTemplate(date: Date) {
-  const calendarConfig = getCalendarConfig(date);
-  const numberOfBox = 42; 
+function dateTemplate(date: Date) {
+  const CalendarConfig = getCalendarConfig(date);
+  const numberOfBox = 42;
 
-  const startDay = calendarConfig.startDay;
-  let divs = Array.from({ length: startDay }).map((_, i) => <div></div>);
+  const startDay = CalendarConfig.startDay;
+  let divs = Array.from({ length: startDay }).map((_, i) => <div key={'preday' + i}></div>); //to be revised
 
-  const numberOfDay = calendarConfig.daysInMonth;
-  Array.from({ length: numberOfDay }).map((_, i) => divs.push(<div>{i+1}</div>));
+  const numberOfDay = CalendarConfig.daysInMonth;
+  Array.from({ length: numberOfDay }).map((_, i) => divs.push(<div key={'day' + i}>{i+1}</div>));
   
   const dayLeft = numberOfBox - numberOfDay - startDay;
-  Array.from({ length: dayLeft }).map((_, i) => divs.push(<div></div>));
+  Array.from({ length: dayLeft }).map((_, i) => divs.push(<div key={'postday' + i}></div>));
 
   return divs;
 }
 
-function monthTemplate(date: Date) {
-  const calendarConfig = getCalendarConfig(date);
-  const month = months[calendarConfig.month];
-  const year = calendarConfig.year;
+function monthYearTemplate(date: Date) {
+  const CalendarConfig = getCalendarConfig(date);
+  const month = months[CalendarConfig.month];
+  const year = CalendarConfig.year;
 
   return <div>{month} {year}</div>;
 }
 
 export default function MyCalendar() {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date());
+
+  const onMonthForward = () => {
+    const nextMonth = addMonths(currentDate, 1);
+    setCurrentDate(nextMonth);
+  }
+
+  const onMonthBackward = () => {
+    const prevMonth = subMonths(currentDate, 1);
+    setCurrentDate(prevMonth);
+  }
+
   return (
     <div>
       <div className='month-container'>
-        <div> left arrow </div>
-        {monthTemplate(todayDate)}
-        <div> right arrow </div>
+        <div onClick={onMonthBackward}><ArrowLeftIcon /></div>
+        {monthYearTemplate(currentDate)}
+        <div onClick={onMonthForward}><ArrowRightIcon /></div>
       </div>
       <div className='date-container'>
         {dayTemplate()}
-        {calendarTemplate(todayDate)}
+        {dateTemplate(currentDate)}
       </div>
     </div>
   )
